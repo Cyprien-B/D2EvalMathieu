10418a99a66431641a6b9cf3704ff8b2021b5fcf
Cyprien <Cyprien.b@protonmail.com>
2025-10-29 23:19:35 +0100

    dernier commit

--- app/src/main/java/com/example/evalmathieud2/ProjectApplication.kt
+++ app/src/main/java/com/example/evalmathieud2/ProjectApplication.kt
@@ -5,6 +5,11 @@ import com.example.evalmathieud2.home.charactersModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
+/**
+ * Classe Application pour l'injection de dépendances Koin.
+ * L'injection est déportée ici pour éviter de perdre les dépendances injectées
+ * lors de la réinitialisation du cycle de vie de MainActivity.
+ */
 class ProjectApplication : Application() {
     override fun onCreate() {
         super.onCreate()
